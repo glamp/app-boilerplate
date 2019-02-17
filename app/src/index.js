ec44f7021628cd13a0e6a7321e7e68f9b943b073
Greg Lamp <lamp.greg@gmial.com>
2019-02-17 08:59:57 -0700

    init

---
+++ app/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+// website styles
+import './index.css';
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// Hot Module Replacement
+if (module.hot) {
+  module.hot.accept();
+}
